53a1a052f1bc9b574193a91e43c4b9a77a54111e
Danford Kija <43994719+Kijacode@users.noreply.github.com>
2020-10-22 21:00:34 +0300

    refactor: db_url

--- config/development.js
+++ config/development.js
@@ -6,7 +6,7 @@ module.exports = {
   api_secret: '5hAOSdq8K4RIxCDhiHs92vkcYho',
   cloudinary_env_variable: "CLOUDINARY_URL=cloudinary://949321177724946:5hAOSdq8K4RIxCDhiHs92vkcYho@dumh64grn",
 
-  db_url : "mongodb+srv://CLOUDINARY_URL=cloudinary://949321177724946:5hAOSdq8K4RIxCDhiHs92vkcYho@dumh64grn",
+  db_url :  "mongodb+srv://Kijacode:vCW0gFVwEvajrvcE@cluster0-zn5n8.mongodb.net/test?retryWrites=true&w=majority",
   port: process.env.PORT || 7000,
 }
 
