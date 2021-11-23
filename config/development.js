f4da1b0d0e0ae7e5598f39696cc3da8e8aa538ee
Danford Kija <43994719+Kijacode@users.noreply.github.com>
2021-11-23 11:50:43 +0300

    remove all secrets

--- config/development.js
+++ config/development.js
@@ -1,12 +1,12 @@
 module.exports = {
-  cloud_name: 'dumh64grn',
+  cloud_name: '',
   
-  api_key: '949321177724946',
+  api_key: '',
 
-  api_secret: '5hAOSdq8K4RIxCDhiHs92vkcYho',
-  cloudinary_env_variable: "CLOUDINARY_URL=cloudinary://949321177724946:5hAOSdq8K4RIxCDhiHs92vkcYho@dumh64grn",
+  api_secret: '',
+  cloudinary_env_variable: "",
 
-  db_url :  "mongodb+srv://Kijacode:vCW0gFVwEvajrvcE@cluster0-zn5n8.mongodb.net/test?retryWrites=true&w=majority",
-  port: process.env.PORT || 7000,
+  db_url :  "",
+  port:,
 }
 
